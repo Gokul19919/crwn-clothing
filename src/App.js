ec90bdf985ccd98e3c4885ca07524de9a2c8e889
Gokul19919 <gokul.illangovan@gmail.com>
2022-01-27 14:02:26 +0530

    updating our shop page with colloection-preview and collection-item component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import './App.css';
 import {Switch, Route} from 'react-router-dom';
-
 import Homepage from './pages/homepage/homepage.component';
-
-const Hatspage = () => (
-  <div>
-    <h1> Hats Page</h1>
-  </div>
-);
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
    <Switch>
     <Route exact path='/' component={Homepage} />
-    <Route path='/hats' component={Hatspage} />
+    <Route path='/shop' component={ShopPage} />
     </Switch>
     </div>
   );
